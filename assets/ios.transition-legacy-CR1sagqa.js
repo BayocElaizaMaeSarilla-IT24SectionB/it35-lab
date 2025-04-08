c7b8ec5212ff30a7665cdbfb302cacc70289eb0f
BayocElaizaMaeSarilla-IT24SectionB <Jseth9561@gmail.com>
2025-04-08 21:57:49 +0800

    Updates

--- assets/ios.transition-legacy-CR1sagqa.js
+++ assets/ios.transition-legacy-CR1sagqa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-YBJsLqpC.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BF2Fpkqc.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
