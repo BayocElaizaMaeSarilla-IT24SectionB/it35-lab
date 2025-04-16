5927957acb0b355451006ba7e5631285b5d120a8
Jenecyl082003 <jenecylsagansan40@gmail.com>
2025-04-17 02:48:31 +0800

    Updates

--- assets/ios.transition-legacy-eJ3VNDJy.js
+++ assets/ios.transition-legacy-eJ3VNDJy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C00rD3jZ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-9g6fwlrT.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
